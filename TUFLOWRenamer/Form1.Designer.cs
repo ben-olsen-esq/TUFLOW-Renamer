437d384324b4baf84d3c3a51053ce179e3bfab1b
Ben Olsen <ben.olsen18@hotmail.com>
2020-05-26 12:36:22 +1000

    Add project files.

---
+++ TUFLOWRenamer/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TUFLOWRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.tbReplace = new System.Windows.Forms.TextBox();
+            this.lbFind = new System.Windows.Forms.Label();
+            this.lbReplace = new System.Windows.Forms.Label();
+            this.tvFolders = new System.Windows.Forms.TreeView();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(48, 6);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(250, 20);
+            this.tbFind.TabIndex = 0;
+            // 
+            // tbReplace
+            // 
+            this.tbReplace.Location = new System.Drawing.Point(365, 6);
+            this.tbReplace.Name = "tbReplace";
+            this.tbReplace.Size = new System.Drawing.Size(250, 20);
+            this.tbReplace.TabIndex = 1;
+            // 
+            // lbFind
+            // 
+            this.lbFind.AutoSize = true;
+            this.lbFind.Location = new System.Drawing.Point(12, 9);
+            this.lbFind.Name = "lbFind";
+            this.lbFind.Size = new System.Drawing.Size(30, 13);
+            this.lbFind.TabIndex = 2;
+            this.lbFind.Text = "Find:";
+            // 
+            // lbReplace
+            // 
+            this.lbReplace.AutoSize = true;
+            this.lbReplace.Location = new System.Drawing.Point(309, 9);
+            this.lbReplace.Name = "lbReplace";
+            this.lbReplace.Size = new System.Drawing.Size(50, 13);
+            this.lbReplace.TabIndex = 3;
+            this.lbReplace.Text = "Replace:";
+            // 
+            // tvFolders
+            // 
+            this.tvFolders.Location = new System.Drawing.Point(13, 41);
+            this.tvFolders.Name = "tvFolders";
+            this.tvFolders.Size = new System.Drawing.Size(609, 479);
+            this.tvFolders.TabIndex = 4;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(540, 525);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 5;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(459, 525);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 6;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Location = new System.Drawing.Point(12, 531);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(96, 13);
+            this.lbPath.TabIndex = 7;
+            this.lbPath.Text = "TUFLOW .tcf Path";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(114, 527);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(258, 20);
+            this.tbPath.TabIndex = 8;
+            this.tbPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPath_KeyDown);
+            this.tbPath.Leave += new System.EventHandler(this.tbPath_Leave);
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(378, 525);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 9;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 561);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.lbPath);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.tvFolders);
+            this.Controls.Add(this.lbReplace);
+            this.Controls.Add(this.lbFind);
+            this.Controls.Add(this.tbReplace);
+            this.Controls.Add(this.tbFind);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(650, 600);
+            this.MinimumSize = new System.Drawing.Size(650, 600);
+            this.Name = "Form1";
+            this.Text = "TUFLOW Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.TextBox tbReplace;
+        private System.Windows.Forms.Label lbFind;
+        private System.Windows.Forms.Label lbReplace;
+        private System.Windows.Forms.TreeView tvFolders;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btBrowse;
+    }
+}
+
---
+++ TUFLOWRenamer/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.IO;
+
+namespace TUFLOWRenamer
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        bool delete;
+        List<string> oldReplacedFiles = new List<string>();
+        List<string> replacedFiles = new List<string>();
+        List<string> filestodelete = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TUFLOW Control files (*.tcf)|*.tcf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Console.WriteLine("This was successful :" + ofd.FileName);
+                    tbPath.Text = ofd.FileName;
+
+                    //Data validation
+
+                    if (path == "")
+                    {
+                        path = tbPath.Text;
+                    }
+                    else if (path == tbPath.Text)
+                        return;
+
+                    path = tbPath.Text;
+
+                    if (!(File.Exists(tbPath.Text)))
+                    {
+                        tbPath.BackColor = Color.Red;
+                        return;
+                    }
+                    else
+                    {
+                        tbPath.BackColor = Color.White;
+                    }
+
+                    UpdateTree();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void UpdateTree()
+        {
+            //Tree Creation Logic
+
+            string folderpath = path;
+            folderpath = folderpath.Replace(Path.GetFileName(path).ToString(), "");
+
+            tvFolders.BeginUpdate();
+            tvFolders.Nodes.Clear();
+            tvFolders.EndUpdate();
+
+            string[] folders = Directory.GetDirectories(folderpath);
+
+            Console.WriteLine("Folders Found:");
+
+            tvFolders.BeginUpdate();
+
+
+            foreach (string s in folders)
+            {
+                
+                tvFolders.Nodes.Add(Path.GetFileName(s).ToString());
+
+                Console.WriteLine("\t" + Path.GetFileName(s).ToString());
+            }
+
+            UpdateNodes(tvFolders.Nodes);
+            UpdateFileNodes(tvFolders.Nodes);
+
+            tvFolders.EndUpdate();
+            tvFolders.ExpandAll();
+
+            Console.WriteLine("Tree Updated");
+        }
+
+        private string GetParentNode(TreeNode n)
+        {
+            if (n.Parent == null)
+                return n.Text;
+            else
+                return  GetParentNode(n.Parent) + @"\" + n.Text;
+        }
+
+        private void UpdateFileNodes(TreeNodeCollection c)
+        {
+            foreach(TreeNode n in c)
+            {
+                string folderpath = Path.Combine(path.Replace(Path.GetFileName(path).ToString(), ""), GetParentNode(n));
+
+                if (!File.Exists(folderpath))
+                {
+                    string[] files = Directory.GetFiles(folderpath);
+
+                    foreach (string s in files)
+                    {
+                        TreeNode nn = new TreeNode();
+                        nn.Text = Path.GetFileName(s).ToString();
+                        nn.ForeColor = Color.Blue;
+                        n.Nodes.Add(nn);
+                    }
+                }
+                UpdateFileNodes(n.Nodes);
+            }
+        }
+
+        private void UpdateNodes(TreeNodeCollection c)
+        {
+            foreach (TreeNode n in c)
+            {
+                string folderpath = Path.Combine(path.Replace(Path.GetFileName(path).ToString(), ""), GetParentNode(n));
+
+                string[] folders = Directory.GetDirectories(folderpath);
+
+                if (!(folders.Length == 0))
+                {
+                    foreach (string s in folders)
+                    {
+                        n.Nodes.Add(Path.GetFileName(s).ToString());
+                    }
+
+                    UpdateNodes(n.Nodes);
+
+                    Console.WriteLine("Sub-Tree Updated");
+                }
+            }
+
+            
+
+        }
+
+        private void GetFileList(ref List<string> sl, TreeNodeCollection c)
+        {
+            foreach(TreeNode n in c)
+            {
+                string filepath = Path.Combine(path.Replace(Path.GetFileName(path).ToString(), ""), GetParentNode(n));
+
+                if (File.Exists(filepath))
+                {
+                    sl.Add(filepath);
+                }
+                else
+                {
+                    GetFileList(ref sl, n.Nodes);
+                }
+            }
+        }
+
+        private void ChangeNodeColors(TreeNodeCollection c)
+        {
+            foreach (TreeNode n in c)
+            {
+                string filepath = Path.Combine(path.Replace(Path.GetFileName(path).ToString(), ""), GetParentNode(n));
+
+                if (File.Exists(filepath))
+                {
+                    foreach(string s in oldReplacedFiles)
+                        if(filepath == s)
+                        {
+                            n.Text = Path.GetFileName(replacedFiles[oldReplacedFiles.FindIndex(a => a.Contains(s))]).ToString();
+                            n.ForeColor = Color.Green;
+                        }
+                    foreach(string s in replacedFiles)
+                        if(s == filepath)
+                        {
+                            delete = true;
+                            filestodelete.Add(filepath);
+                            n.ForeColor = Color.Red;
+                        }
+                }
+                else
+                {
+                    ChangeNodeColors(n.Nodes);
+                }
+            }
+        }
+
+        private void tbPath_Leave(object sender, EventArgs e)
+        {
+            UpdateTree();
+        }
+
+        private void tbPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                UpdateTree();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            UpdateTree();
+
+            string find = tbFind.Text;
+            string replace = tbReplace.Text;
+
+            if (find.Length == 0 || replace.Length == 0)
+                return;
+
+            List<string> files = new List<string>();
+            oldReplacedFiles.Clear();
+            replacedFiles.Clear();
+            filestodelete.Clear();
+            delete = false;
+
+
+            GetFileList(ref files, tvFolders.Nodes);
+
+            foreach (string s in files)
+            {
+                if (Path.GetFileName(s).Contains(find))
+                {
+                    oldReplacedFiles.Add(s);
+                    string file = Path.Combine(Path.GetDirectoryName(s),Path.GetFileName(s).ToString().Replace(find,replace));
+                    replacedFiles.Add(file);
+                }
+            }
+
+            tvFolders.BeginUpdate();
+            ChangeNodeColors(tvFolders.Nodes);
+            tvFolders.EndUpdate();
+
+            Console.WriteLine("Node Colors Updated.");
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (delete)
+            {
+                var confirmResult = MessageBox.Show("There are files that have matching names, would you like to delete them?",
+                                         "Confirm Delete", MessageBoxButtons.YesNo);
+
+                if (confirmResult == DialogResult.No)
+                    return;
+
+                foreach (string s in filestodelete)
+                    File.Delete(s);
+            }
+
+            for(int x = 0; x < replacedFiles.Count; x++)
+            {
+                File.Move(oldReplacedFiles[x], replacedFiles[x]);
+            }
+
+            MessageBox.Show("Files Renamed Successfully!");
+
+            replacedFiles.Clear();
+            oldReplacedFiles.Clear();
+            filestodelete.Clear();
+        }
+    }
+}
